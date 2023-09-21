dae2170da7ebf5c69bd6727ab2ea18c69d127456
MrChae1 <ljnellam27@gmail.com>
2023-09-22 01:04:12 +0700

    Adding functionality to markTurn Factory that display the mark of the player in the button

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function gameBoard(){
 
+  const markTurn = (turnData, playerTurn, arrbtn) => {
+    for(const newArr of arrbtn){
+      let dataAtt = newArr.getAttribute('data-num');
+      if (dataAtt === turnData){
+        let x = document.querySelector(`button[data-num="${turnData}"]`);
+        x.textContent = playerTurn;   
+      }
+    }
+  }
+
+  return { markTurn }
 
 }
 
@@ -10,13 +21,16 @@ function gameStart(){
   let clickedButton = event.currentTarget.getAttribute('data-num');
   playRound(clickedButton);
   })); 
+
+
+  const board = gameBoard();
   
   const player = [
     {
-      playerOne: 'X'
+      players: 'X'
     },
     {
-      playerTwo: 'O'
+      players: 'O'
     }
   ];
 
@@ -28,22 +42,25 @@ function gameStart(){
   const getActivePlayer = () => activePlayer;
 
   const playRound = (data) => {
-    console.log(data);
-    
+    board.markTurn(data, getActivePlayer().players, allButton)
+
+    switchPlayerTurn();
   }
 
 }
 
-function clickevent (){
-  let allButton = Array.from(document.querySelectorAll('.box-container'));
-  // Get what button I click
-  const clickBtn = allButton.forEach(button => button.addEventListener('click', event => {
-  let clickedButton = event.currentTarget.getAttribute('data-num');
-  toStartGame.playRound(clickedButton);
-  }));  
-}
-
 const game = gameStart();
+
+
+
+// Task Tommorrow
+// give validation that the turn can't change or click again.
+// display the winner
+// the button X and O to know who gonna be the first turn
+// computer player
+// 
+
+
 
 
 
